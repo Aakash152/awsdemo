59c0ac95e981c45d70fcea0d4cc9febf31550f5b
Aakash152 <aakashkharche152@gmail.com>
2020-10-06 16:17:25 +0530

    back to 2nd commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from "react";
 import "./App.css";
-import Routes from "./routes/Routes";
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import { MovieProvider } from "./context/MovieProvider";
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <MovieProvider>
-        <Routes />
-      </MovieProvider>
-=======
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -23,10 +17,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and AWS
         </a>
       </header>
->>>>>>> parent of 87b2083... Second commit chnages in app.js and index.html
     </div>
   );
 }
