3410c3829e2b769e4dc5500619805333159a4d6c
Aakash152 <aakashkharche152@gmail.com>
2020-10-06 16:07:21 +0530

    back to 2nd commit

--- src/App.js
+++ src/App.js
@@ -7,9 +7,26 @@ import { MovieProvider } from "./context/MovieProvider";
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <MovieProvider>
         <Routes />
       </MovieProvider>
+=======
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> parent of 87b2083... Second commit chnages in app.js and index.html
     </div>
   );
 }
