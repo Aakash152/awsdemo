ae2c7b3b035d1e6e29a35e9cdd110168646c20b5
Aakash152 <aakashkharche152@gmail.com>
2020-10-08 18:16:43 +0530

    New Login Component is added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./components/Home";
+//import Home from "./components/Home";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Home />
+        {/* <Home /> */}
+        <Login/>
       </header>
     </div>
   );
