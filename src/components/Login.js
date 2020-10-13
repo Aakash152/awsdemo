fec0e4ff79a29980baf6969147604fc43be2cb4c
Aakash152 <aakashkharche152@gmail.com>
2020-10-13 17:00:04 +0530

    Validations in Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,7 @@ import {
   FormControl,
   Input,
   InputLabel,
+  FormHelperText,
 } from "@material-ui/core";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import withStyles from "@material-ui/core/styles/withStyles";
@@ -44,7 +45,9 @@ const styles = (theme) => ({
     marginTop: theme.spacing(3),
   },
 });
-
+const EmailPattern = new RegExp(
+  /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+);
 function Login(props) {
   let history = useHistory();
   const { classes, LoginUser } = props;
@@ -57,11 +60,41 @@ function Login(props) {
   // const [email, setEmail] = useState("");
   // const [password, setPassword] = useState("");
 
+  const [LoginErrors, SetLoginErrors] = useState({
+    emailError: "",
+    passwordError: "",
+  });
+
   const HandleChange = (e) => {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case "email":
+        LoginErrors.emailError =
+          value.length === 0
+            ? "Required"
+            : EmailPattern.test(value)
+            ? ""
+            : "Please Enter Valid Email Address";
+        break;
+      case "password":
+        LoginErrors.passwordError =
+          value.length === 0
+            ? "Required"
+            : value.length < 4 || value.length > 10
+            ? "Please Enter Password between 4 to 10 Characters"
+            : "";
+        break;
+      default:
+        break;
+    }
+
     SetLoginData({
       ...LoginData,
       [e.target.name]: e.target.value,
     });
+    SetLoginErrors(LoginErrors);
+    console.log(LoginErrors);
   };
 
   const HandleSubmit = (e) => {
@@ -70,6 +103,12 @@ function Login(props) {
     LoginUser();
     history.push("/home");
   };
+
+  const DisableLoginbtn =
+    (LoginData.email.length && LoginData.password.length) === 0 ||
+    LoginErrors.emailError ||
+    LoginErrors.passwordError;
+
   return (
     <main className={classes.main}>
       <Paper className={classes.paper}>
@@ -90,6 +129,9 @@ function Login(props) {
               value={LoginData.email}
               onChange={HandleChange}
             />
+            <FormHelperText style={{ color: "red", fontSize: "15px" }}>
+              {LoginErrors.emailError}
+            </FormHelperText>
           </FormControl>
           <FormControl margin="normal" required fullWidth>
             <InputLabel htmlFor="password">Password</InputLabel>
@@ -101,10 +143,14 @@ function Login(props) {
               value={LoginData.password}
               onChange={HandleChange}
             />
+            <FormHelperText style={{ color: "red", fontSize: "15px" }}>
+              {LoginErrors.passwordError}
+            </FormHelperText>
           </FormControl>
           <Button
             type="submit"
             fullWidth
+            disabled={DisableLoginbtn}
             variant="contained"
             color="primary"
             className={classes.submit}
